3501d6ffed55cdb6ed8965954dfe7408dcfb9cbd
AYUSH MISHRA <ayushmishra0919@gmail.com>
2026-06-22 21:34:05 +0530

    Time: 5 ms (44.25%), Space: 96.9 MB (27.35%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+        
+        // 1. Find middle - slow ends up at start of 2nd half
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        // 2. Reverse second half
+        ListNode secondHalf = reverse(slow.next);
+        
+        // 3. Compare first half and reversed second half
+        ListNode p1 = head, p2 = secondHalf;
+        boolean result = true;
+        while (p2 != null) { // p2 is shorter or equal length
+            if (p1.val != p2.val) {
+                result = false;
+                break;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        
+        // Optional: restore the list back
+        slow.next = reverse(secondHalf);
+        
+        return result;
+    }
+    
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
